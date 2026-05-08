aab6f5ebeb170b779e04bca8a271e17b8e529b69
Sandor Maro <marosandor2001@gmail.com>
2026-05-08 22:29:25 +0200

    Create DTO classes

--- backend/src/main/java/org/marosandor/backend/dto/report/ReportRequest.java
+++ backend/src/main/java/org/marosandor/backend/dto/report/ReportRequest.java
@@ -1,4 +1,26 @@
 package org.marosandor.backend.dto.report;
 
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import org.marosandor.backend.enums.Category;
+import java.math.BigDecimal;
+
+@Data
 public class ReportRequest {
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    private Category category;
+
+    @NotNull
+    private BigDecimal latitude;
+
+    @NotNull
+    private BigDecimal longitude;
+
+    private boolean isAnonymous = false;
 }
--- backend/src/main/java/org/marosandor/backend/dto/report/ReportResponse.java
+++ backend/src/main/java/org/marosandor/backend/dto/report/ReportResponse.java
@@ -1,4 +1,21 @@
 package org.marosandor.backend.dto.report;
 
+import lombok.*;
+import org.marosandor.backend.enums.Category;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
 public class ReportResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private Category category;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private String statusLabel;
+    private boolean isAnonymous;
+    private String username;
+    private LocalDateTime createdAt;
 }
